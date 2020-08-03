c11674c27837561673b61207f0e21cabf660faa5
borbalagera <borbalagera@gmail.com>
2020-08-03 17:59:55 +0200

    Updates

---
+++ precache-manifest.31ed4c8ec039c0a6bdc939dff787f9e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee04ea3f194d43f952a3a61410f18ea",
+    "url": "/storeApp/index.html"
+  },
+  {
+    "revision": "a2ecf2bba8457fe769e9",
+    "url": "/storeApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fc996b150caa99ed2ced",
+    "url": "/storeApp/static/css/main.2c8f0ad4.chunk.css"
+  },
+  {
+    "revision": "a2ecf2bba8457fe769e9",
+    "url": "/storeApp/static/js/2.96e23bfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/storeApp/static/js/2.96e23bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc996b150caa99ed2ced",
+    "url": "/storeApp/static/js/main.6845d976.chunk.js"
+  },
+  {
+    "revision": "c446d12ce8674b49b697",
+    "url": "/storeApp/static/js/runtime-main.e8aa73d5.js"
+  }
+]);
